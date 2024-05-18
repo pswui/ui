07ad04f8326e0fc206f9755a66ad11b80f034948
p-sw <shinwoo.park@psw.kr>
2024-05-18 21:26:58 +0900

    feat: add stories for new variants in button

--- packages/react/stories/Button.stories.ts
+++ packages/react/stories/Button.stories.ts
@@ -5,8 +5,12 @@ const meta: Meta<typeof Button> = {
   component: Button,
   tags: ["autodocs"],
   argTypes: {
-    variant: { options: ["default", "ghost"], control: "select" },
+    variant: {
+      options: ["default", "ghost", "outline", "link"],
+      control: "select",
+    },
     size: { options: ["sm", "md", "lg"], control: "select" },
+    onClick: { table: { disable: true } },
   },
   args: { children: "Button", onClick: () => console.log("clicked") },
 };
@@ -17,6 +21,27 @@ type Story = StoryObj<typeof Button>;
 export const Default: Story = {
   args: {
     variant: "default",
+    size: "md",
+  },
+};
+
+export const Ghost: Story = {
+  args: {
+    variant: "ghost",
+    size: "md",
+  },
+};
+
+export const Outline: Story = {
+  args: {
+    variant: "outline",
+    size: "md",
+  },
+};
+
+export const Link: Story = {
+  args: {
+    variant: "link",
     size: "md",
   },
 };
