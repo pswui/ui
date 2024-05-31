5d0ee1409889e195c4a804416479ec8f310f2d41
p-sw <shinwoo.park@psw.kr>
2024-05-31 21:08:54 +0900

    feat: add mdx

--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "tailwindcss";
+import mdx from '@mdx-js/rollup';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), mdx()],
   css: {
     postcss: {
       plugins: [tailwindcss()],
