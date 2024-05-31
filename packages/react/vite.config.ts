c1bfe4bb133a648cd6c191719ed282bdd362166b
p-sw <shinwoo.park@psw.kr>
2024-06-01 02:32:27 +0900

    feat: add rehypeHighlight plugin in mdx

--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -3,10 +3,11 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "tailwindcss";
 import mdx from '@mdx-js/rollup';
 import { resolve } from 'node:path';
+import rehypeHighlight from "rehype-highlight";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), mdx()],
+  plugins: [react(), mdx({ rehypePlugins: [rehypeHighlight] })],
   css: {
     postcss: {
       plugins: [tailwindcss()],
