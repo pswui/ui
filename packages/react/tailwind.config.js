fb044c9bdb173f231afa2ada0ea115405fd91c8f
p-sw <shinwoo.park@psw.kr>
2024-05-31 21:09:32 +0900

    fix: change dark mode system

--- packages/react/tailwind.config.js
+++ packages/react/tailwind.config.js
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./{components,stories,src}/**/*.{js,jsx,ts,tsx,css}"],
-  darkMode: "selector",
+  darkMode: ["variant", [
+    '@media (prefers-color-scheme: dark) { &:is(.system *) }',
+    '&:is(.dark *)'
+  ]],
   theme: {
     extend: {},
   },
