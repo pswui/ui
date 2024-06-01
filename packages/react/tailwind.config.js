3343957ad75b5226487650f5d362c87914085684
p-sw <shinwoo.park@psw.kr>
2024-06-02 07:26:29 +0900

    fix: add mdx in tailwind config's target

--- packages/react/tailwind.config.js
+++ packages/react/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./{components,stories,src}/**/*.{js,jsx,ts,tsx,css}"],
+  content: ["./{components,stories,src}/**/*.{js,jsx,ts,tsx,css,mdx}"],
   darkMode: ["variant", [
     '@media (prefers-color-scheme: dark) { &:is(.system *) }',
     '&:is(.dark *)'
