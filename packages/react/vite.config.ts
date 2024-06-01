a4315420190d0afb0187e4fbb815c9736196b6a9
p-sw <shinwoo.park@psw.kr>
2024-06-02 01:53:45 +0900

    feat: add remark-gfm to support table

--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -4,10 +4,11 @@ import tailwindcss from "tailwindcss";
 import mdx from '@mdx-js/rollup';
 import { resolve } from 'node:path';
 import rehypeHighlight from "rehype-highlight";
+import remarkGfm from "remark-gfm";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), mdx({ rehypePlugins: [rehypeHighlight] })],
+  plugins: [react(), mdx({ rehypePlugins: [rehypeHighlight], remarkPlugins: [remarkGfm] })],
   css: {
     postcss: {
       plugins: [tailwindcss()],
