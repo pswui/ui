edfaef2c7524d198ed1b6bcea2c7bf4781467957
p-sw <shinwoo.park@psw.kr>
2024-06-11 15:43:56 +0900

    feat: add lib alias in vite config and tsconfig
    The commit includes a new alias @pswui-lib in both the `vite.config.ts` and `tsconfig.json` files. This change facilitates a more straightforward and concise import of files located in the 'lib' directory.

--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -1,17 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "tailwindcss";
-import mdx from '@mdx-js/rollup';
-import { resolve } from 'node:path';
+import mdx from "@mdx-js/rollup";
+import { resolve } from "node:path";
 import remarkGfm from "remark-gfm";
-import withSlug from "rehype-slug"
+import withSlug from "rehype-slug";
 import withToc from "@stefanprobst/rehype-extract-toc";
 import withTocExport from "@stefanprobst/rehype-extract-toc/mdx";
-import dynamicImport from 'vite-plugin-dynamic-import'
+import dynamicImport from "vite-plugin-dynamic-import";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), mdx({ rehypePlugins: [withSlug, withToc, withTocExport], remarkPlugins: [remarkGfm] }), dynamicImport()],
+  plugins: [
+    react(),
+    mdx({
+      rehypePlugins: [withSlug, withToc, withTocExport],
+      remarkPlugins: [remarkGfm],
+    }),
+    dynamicImport(),
+  ],
   css: {
     postcss: {
       plugins: [tailwindcss()],
@@ -19,8 +26,9 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@components': resolve(__dirname, './components'),
-      '@': resolve(__dirname, './src'),
-    }
-  }
+      "@components": resolve(__dirname, "./components"),
+      "@": resolve(__dirname, "./src"),
+      "@pswui-lib": resolve(__dirname, "./lib"),
+    },
+  },
 });
