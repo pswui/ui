447b2249c887addfb5ba9226ebc46a4cd19997fe
Shinwoo PARK <devpysweb@gmail.com>
2024-06-13 19:34:05 +0000

    refactor: remove useless dependencies for old docs

--- packages/react/tailwind.config.js
+++ packages/react/tailwind.config.js
@@ -11,5 +11,4 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/typography"), require("tailwind-scrollbar")],
 };
--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -1,23 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "tailwindcss";
-import mdx from "@mdx-js/rollup";
 import { resolve } from "node:path";
-import remarkGfm from "remark-gfm";
-import withSlug from "rehype-slug";
-import withToc from "@stefanprobst/rehype-extract-toc";
-import withTocExport from "@stefanprobst/rehype-extract-toc/mdx";
-import dynamicImport from "vite-plugin-dynamic-import";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    mdx({
-      rehypePlugins: [withSlug, withToc, withTocExport],
-      remarkPlugins: [remarkGfm],
-    }),
-    dynamicImport(),
+    react()
   ],
   css: {
     postcss: {
