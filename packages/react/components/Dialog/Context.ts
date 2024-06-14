22ab752b75b7841dd351dc533907b7071986fbd1
p-sw <shinwoo.park@psw.kr>
2024-06-14 23:59:50 +0900

    fix: split component file

---
+++ packages/react/components/Dialog/Context.ts
@@ -0,0 +1,27 @@
+import { Dispatch, SetStateAction, useContext, createContext } from "react";
+
+/**
+ * =========================
+ * DialogContext
+ * =========================
+ */
+
+export interface IDialogContext {
+  opened: boolean;
+}
+
+export const initialDialogContext: IDialogContext = { opened: false };
+export const DialogContext = createContext<
+  [IDialogContext, Dispatch<SetStateAction<IDialogContext>>]
+>([
+  initialDialogContext,
+  () => {
+    if (process.env.NODE_ENV && process.env.NODE_ENV === "development") {
+      console.warn(
+        "It seems like you're using DialogContext outside of a provider.",
+      );
+    }
+  },
+]);
+
+export const useDialogContext = () => useContext(DialogContext);
