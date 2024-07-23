c8f1912bae3a6709ae35eb32107aef2128f1dc01
p-sw <shinwoo.park@psw.kr>
2024-07-23 18:06:18 +0900

    chore: make dev env dark mode work properly

--- packages/react/tailwind.config.js
+++ packages/react/tailwind.config.js
@@ -1,13 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./{components,stories,src}/**/*.{js,jsx,ts,tsx,css,mdx}"],
-  darkMode: [
-    "variant",
-    [
-      "@media (prefers-color-scheme: dark) { &:is(.system *) }",
-      "&:is(.dark *)",
-    ],
-  ],
+  darkMode: "media",
   theme: {
     extend: {},
   },
