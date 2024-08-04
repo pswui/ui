3d55a988215d562d313b974d4c2753e43433aa92
p-sw <shinwoo.park@psw.kr>
2024-08-04 16:23:53 +0900

    fix: add cache option no-cache to prevent caching

--- packages/cli/src/helpers/safe-fetcher.ts
+++ packages/cli/src/helpers/safe-fetcher.ts
@@ -4,7 +4,7 @@ export async function safeFetch(
   | { message: string; ok: false; response: Response }
   | { ok: true; response: Response }
 > {
-  const response = await fetch(url);
+  const response = await fetch(url, { cache: "no-cache" });
   if (response.ok) {
     return {
       ok: true,
