10623113494be523f361697df8a036147e6dbf3b
Shinwoo PARK <shinwoo.park@psw.kr>
2026-04-15 04:16:58 +0900

    Revert "fix(select): replace text chevron with svg icon"

--- packages/react/components/Select.tsx
+++ packages/react/components/Select.tsx
@@ -310,21 +310,7 @@ const Select = React.forwardRef<HTMLButtonElement, SelectProps>(
               opened ? "rotate-180" : ""
             }`}
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              className="size-full"
-            >
-              <title>Open options</title>
-              <path
-                d="m6 9 6 6 6-6"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
+            v
           </span>
         </button>
         <div
